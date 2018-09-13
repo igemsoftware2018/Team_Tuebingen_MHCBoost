e1a88b3769c0be4685b914bbd1848eda6a11d4d1
Zethson <lukas.heumos@gmail.com>
2018-09-13 19:59:24 +0200

    [FEATURE] Prototyp kfold cv

--- src/algorithms/ml/gbtrees/gbtrees_training.py
+++ src/algorithms/ml/gbtrees/gbtrees_training.py
@@ -1,8 +1,9 @@
 import logging
 import warnings
 
-from src.algorithms.ml.encoder import encode_training_data
-from src.model.encoding.extended_blomap import extended_blomap_dict
+from xgboost import XGBClassifier
+
+from src.algorithms.evaluation.k_fold_crossvalidation import k_fold_cross_validation
 
 console = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -12,8 +13,12 @@ LOG.addHandler(console)
 LOG.setLevel(logging.INFO)
 
 
-def gbtrees_train(peptides):
+def gbtrees_train(encoded_aminoacids, binding_values):
     # see: https://stackoverflow.com/questions/49545947/sklearn-deprecationwarning-truth-value-of-an-array
     warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
-    encoded_aminoacids, binding_values = encode_training_data(peptides, extended_blomap_dict, "extended blomap")
+    classifier = XGBClassifier()
+    k_fold_cross_validation(encoded_aminoacids, binding_values, classifier, 10)
+
+
+
